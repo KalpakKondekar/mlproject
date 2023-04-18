84a09412bccc41f7923112c88a1953065e2ee630
KalpakKondekar <kalpak9370@gmail.com>
2023-04-18 19:24:37 +0530

    EDA and Problems statements

--- src/exception.py
+++ src/exception.py
@@ -1,8 +1,10 @@
 import sys
-import logging
+import logging 
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
+    file_name=exc_tb.tb_frame.f_code.co_filename
     file_name=exc_tb.tb_frame.f_code.co_filename
     error_message="Error occered in pytthn script name [{0}] line number [{1}] error message[{2}]".format(
         file_name,exc_tb.tb_lineno,str(error))
